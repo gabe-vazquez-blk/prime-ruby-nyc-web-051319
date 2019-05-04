070d6958dfea7f0360cd79963656e3ca834bc501
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-04 15:13:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 # Add  code here!
-def prime?(n)
-  a = []
-  for d in 2..(n - 1)
-    foundDivider = ((n % d) == 0)
-    a.push(foundDivider)
-  end
-  if a.include?(true) == true
-    false
-  else
-    true
-  end
-end
+#def prime?(n)
+  #a = []
+  #for d in 2..(n - 1)
+    #foundDivider = ((n % d) == 0)
+    #a.push(foundDivider)
+  #end
+  #if a.include?(true) == true
+    #false
+  #else
+    #true
+  #end
+#end
