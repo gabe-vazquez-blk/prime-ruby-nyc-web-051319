bb51382d3920304dd924035eaed2345b9184164f
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-04 15:06:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 # Add  code here!
 def prime?(n)
-  foundDivider = false
+  #foundDivider = false
   a = []
   for d in 2..(n - 1)
-    foundDivider = ((n % d) == 0) || foundDivider
+    foundDivider = ((n % d) == 0) || false
     a.push(foundDivider)
   end
   if a.include?(true) == true
