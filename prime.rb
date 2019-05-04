ad282272c931e9279db7920da70622c68bf2765a
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-04 15:20:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -18,4 +18,10 @@ def prime?(num)
       return false if num % n == 0
     end
     true
+  elsif num <= 0
+    (-2..(-num + 1)).each do |n|
+      return false if num % n == 0
+    end
+    true
+  end
 end
